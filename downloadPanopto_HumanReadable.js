85ea26d731e27580524dbc4b1a1ae4d5a4aadda8
dfaour <dfaour@gmail.com>
2017-11-16 17:45:22 -0400

    Add files via upload

---
+++ downloadPanopto_HumanReadable.js
@@ -0,0 +1,18 @@
+javascript:(function()
+	{
+	if(window.location.href.indexOf("panopto.com") < 1) 
+	{ 
+		alert("No Panopto video detected!"); 
+		exit(); 
+	}
+	var metas = document.getElementsByTagName('meta'); 
+	for (var i=0; i<metas.length; i++) 
+		if (metas[i].getAttribute("name") == "twitter:player:stream") 
+		{ 
+			console.log(metas[i].getAttribute("content"));
+			window.open(metas[i].getAttribute("content").split('?')[0]);
+		} 
+	}
+	/*v1.0,20171116,DF,GPLv3*/
+)();
+
